3e617f86fb37933f85a3c7d44be3fbb0f9871b05
Michal <238181@student.pwr.edu.pl>
2018-01-19 06:04:35 +0100

    Added method responsible for saving GIF to file

--- src/me/mjaroszewicz/DataUtils.java
+++ src/me/mjaroszewicz/DataUtils.java
@@ -1,12 +1,13 @@
 package me.mjaroszewicz;
 
+import javax.imageio.stream.FileImageOutputStream;
+import javax.imageio.stream.ImageOutputStream;
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.DataFlavor;
-import java.awt.datatransfer.Transferable;
-import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class DataUtils {
 
@@ -16,8 +17,27 @@ public class DataUtils {
 
         Clipboard c = Toolkit.getDefaultToolkit().getSystemClipboard();
 
-
         c.setContents(new TransferableImage(bi), null);
+    }
+
+    public static void saveSequenceAsGif(List<BufferedImage> list, String filename)throws IOException{
+
+        BufferedImage firstImage = list.get(0);
+
+        ImageOutputStream os = new FileImageOutputStream(new File(filename));
+
+        GifSequenceWriter writer = new GifSequenceWriter(firstImage, os);
+
+        writer.writeToSequence(firstImage);
+
+        for(int i = 1; i < list.size() - 1; i++){
+            BufferedImage next = list.get(i);
+            writer.writeToSequence(next);
+        }
+
+        writer.close();
+        os.close();
+
     }
 }
 
